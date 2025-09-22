3bd32f83e65591e3648ed0495d74eaf48acdc03a
Rokurook <ma.requena@duocuc.cl>
2025-09-21 22:48:59 -0300

    se agrega modelo/class inventario

--- tienda/core/models.py
+++ tienda/core/models.py
@@ -16,6 +16,14 @@ class Producto(models.Model):
     def __str__ (self):
         return self.nombre
     
+    class Inventario(models.Model):
+        nombre = models.CharField(max_length=100)
+        identificacion = models.CharField(max_length=50, unique=True)  
+        cantidad = models.PositiveIntegerField()  
+        categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.cantidad})"
     
 
     
