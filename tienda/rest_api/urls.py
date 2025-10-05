731f7acf1cd181e4de128e141f65d36956f93e83
Squawks1 <to.toledom@duocuc.cl>
2025-10-04 21:09:25 -0300

    update urls.py rest_api
    se agregan los directorios de las urls en la app rest_api

---
+++ tienda/rest_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from rest_api.viewsLogin import login, perfil_usuario
+
+urlpatterns = [
+    path('categorias/', views.lista_Categoria, name = "lista_Categoria"),
+    path('productos/', views.lista_Producto, name = "lista_Producto"),
+    path('inventario/', views.lista_Inventario, name = "lista_Inventario"),
+    path('categorias/<int:id>/', views.detalle_Categoria, name = "detalle_Categoria"),
+    path('productos/<int:id>/', views.detalle_Producto, name = "detalle_Producto"),
+    path('inventario/<int:id>/', views.detalle_Inventario, name = "detalle_Inventario"),
+    path('login/', login, name = "login"),
+    path('perfil/', perfil_usuario, name = "perfil")
+]
