7bc79f1b5d3d32f6fb2e1bcdffde9125b7b3eb2a
Rokurook <ma.requena@duocuc.cl>
2025-10-05 23:18:19 -0300

    modificacion  res/api2

--- tienda/core/urls.py
+++ tienda/core/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     #path('libro/<int:id>/editar/', views.editar_categoria, name='editar_categoria'),
     path('api/categorias', views.listar_categorias, name='listar_categorias'),
     path('api/productos', views.listar_productos, name='listar_productos'),
-    path('api/productos/categoria/<int:categoria_id>', views.listar_productos_por_categoria, name='listar_productos_por_categoria'),
-    path('api/auth/login', viewsLogin.login, name='login'),
-    path('api/auth/perfil', viewsLogin.perfil_usuario, name='perfil'),
+    #path('api/productos/categoria/<int:categoria_id>', views.listar_productos_por_categoria, name='listar_productos_por_categoria'),
+    #path('api/auth/login', viewsLogin.login, name='login'),
+    #path('api/auth/perfil', viewsLogin.perfil_usuario, name='perfil'),
     #path('api/inventario', listar_inventario, name='listar_inventario'),
     path('api/', include('rest_api.urls'))
-]
